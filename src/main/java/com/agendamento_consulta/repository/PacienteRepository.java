6eeb8d06fb4a91143b1c77e9b0d3845fd770aebc
Raul Renshaw <raulrenshaw@gmail.com>
2025-08-13 21:36:50 -0300

    add: repository de todas as entitys

---
+++ src/main/java/com/agendamento_consulta/repository/PacienteRepository.java
@@ -0,0 +1,9 @@
+package com.agendamento_consulta.repository;
+
+import jdk.jfr.Registered;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PacienteRepository extends JpaRepository {
+}
