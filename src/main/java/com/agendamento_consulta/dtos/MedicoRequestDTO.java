44fa691281b5f76aae365150b4a161339e176b8f
Raul <raulrenshaw@gmail.com>
2025-08-16 17:43:23 -0300

    fix: medico request end mappper

--- src/main/java/com/agendamento_consulta/dtos/MedicoRequestDTO.java
+++ src/main/java/com/agendamento_consulta/dtos/MedicoRequestDTO.java
@@ -1,6 +1,6 @@
 package com.agendamento_consulta.dtos;
 
-import jakarta.persistence.Column;
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import lombok.*;
@@ -10,32 +10,20 @@ import lombok.*;
 @NoArgsConstructor
 public class MedicoRequestDTO {
 
-    @Column(nullable = false)
-    @Getter
-    @Setter
     @NotNull
     private String nome;
 
-    @Column(unique = true, nullable = false)
-    @Getter
-    @Setter
     @NotNull
     private String crm;
 
-    @Column(nullable = false)
-    @Getter
-    @Setter
     @NotNull
     private String specialty;
 
-    @Pattern(regexp = "\\d{11}", message = "O número deve conter exatamente 11 números")
-    @Getter
-    @Setter
     @NotNull
+    @Pattern(regexp = "\\d{11}", message = "O número deve conter exatamente 11 números")
     private String telefone;
 
-    @Getter
-    @Setter
     @NotNull
+    @Email(message = "E-mail inválido")
     private String email;
 }
--- src/main/java/com/agendamento_consulta/mapper/MedicoMapper.java
+++ src/main/java/com/agendamento_consulta/mapper/MedicoMapper.java
@@ -13,7 +13,6 @@ import org.springframework.stereotype.Component;
 
 import java.util.List;
 
-@Component
 @Mapper(componentModel = "spring")
 public interface MedicoMapper {
 
