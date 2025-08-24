b2acb6edbf29e1bd1212f514f102db909a228c3a
Raul <raulrenshaw@gmail.com>
2025-08-24 19:08:00 -0300

    add: Emtitys role e users

--- src/main/java/com/agendamento_consulta/model/Role.java
+++ src/main/java/com/agendamento_consulta/model/Role.java
@@ -1,9 +1,35 @@
 package com.agendamento_consulta.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
-@Table
+@Table(name = "roles")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long roleId;
+
+    private String name;
+
+    public enum Values{
+        BASIC(1L),
+        ADMIN(2L);
+        Long roleId;
+
+        Values(Long roleid){
+            this.roleId = roleid;
+        }
+        public long getRoleId(){
+            return roleId;
+        }
+    }
 }
--- src/main/java/com/agendamento_consulta/model/Users.java
+++ src/main/java/com/agendamento_consulta/model/Users.java
@@ -1,6 +1,10 @@
 package com.agendamento_consulta.model;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.util.HashSet;
 import java.util.Set;
@@ -8,15 +12,25 @@ import java.util.UUID;
 
 @Entity
 @Table(name = "users")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Users {
 
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "user_id")
     private UUID id;
-
+    @Column(unique = true)
     private String username;
     private String password;
-
+    @ManyToMany(fetch = FetchType.EAGER, cascade = {CascadeType.MERGE, CascadeType.PERSIST})
+    @JoinTable(
+            name = "users_roles",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_Id")
+    )
     private Set<Role> roles = new HashSet<>();
 
 }
